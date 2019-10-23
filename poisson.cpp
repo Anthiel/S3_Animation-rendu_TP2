9d4dbe8bf504dfffc49446f7bb4d2f3dfa30a207
Ptilulu83 <lucas.moragues@wanadoo.fr>
2019-10-23 14:58:25 +0200

    poisson suite

--- poisson.cpp
+++ poisson.cpp
@@ -2,6 +2,24 @@
 
 Poisson::Poisson()
 {
+    this->position={0,0,0};
+    this->vitesse={0,0,0};
+    this->size=1;
+}
+
+
+Poisson::Poisson(QVector3D position, QVector3D vitesse, float size)
+{
+    this->position=position;
+    this->vitesse=vitesse;
+    this->size=size;
+}
+
+bool Poisson::dans_voisinage( QVector3D p){
 
 }
 
+void Poisson::animate(float dt)
+{
+    this->position+=dt*vitesse;
+}
--- poisson.h
+++ poisson.h
@@ -1,11 +1,31 @@
 #ifndef POISSON_H
 #define POISSON_H
 
+#include <QVector3D>
+#include <QOpenGLShaderProgram>
+
+#include <QOpenGLBuffer>
+#include <cmath>
 
 class Poisson
 {
 public:
     Poisson();
+    Poisson(QVector3D position, QVector3D vitesse, float size);
+
+    void animate(float dt);
+    bool dans_voisinage(QVector3D p);
+    //void display(QOpenGLShaderProgram *program_particule);
+
+
+
+
+
+
+    QVector3D position,vitesse;
+    float size;
+    /*lifeTimeLeft,initialLifeTime;
+    QMatrix4x4 modelMatrixParticule;*/
 };
 
 #endif // POISSON_H
