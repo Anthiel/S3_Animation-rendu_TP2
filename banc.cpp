7dea9e500aaefb25e266103e101219268e16bb69
Antoine <Antoine.thiel83@gmail.com>
2019-10-23 14:59:18 +0200

    banc suite

--- banc.cpp
+++ banc.cpp
@@ -4,3 +4,16 @@ Banc::Banc()
 {
 
 }
+
+void Banc::initPoissons(){
+    for(int i = 0; i < nbPoissons; i++){
+        Poisson p;
+        double posX = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
+        double posY = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
+        double posZ = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
+
+        QVector3D position(posX, posY, posZ);
+        p.position = position;
+        vecPoisson.push_back(p);
+    }
+}
--- banc.h
+++ banc.h
@@ -1,11 +1,22 @@
 #ifndef BANC_H
 #define BANC_H
 
+#include "poisson.h"
+
+#include <vector>
+#include <QVector3D>
 
 class Banc
 {
 public:
     Banc();
+    int nbPoissons = 0;
+    std::vector<Poisson> vecPoisson;
+
+protected:
+    double rangePos = 1000.0;
+    double rangeNeg = -1000.0;
+    int coefDivision = 100;
 };
 
 #endif // BANC_H
