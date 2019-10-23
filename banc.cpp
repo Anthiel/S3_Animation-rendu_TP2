a6a3f9706c6c97d9f6a542c0a24f88b0f0914945
Antoine <Antoine.thiel83@gmail.com>
2019-10-23 15:35:45 +0200

    banc test

--- banc.cpp
+++ banc.cpp
@@ -7,12 +7,16 @@ Banc::Banc()
 
 void Banc::initPoissons(QVector3D vitesse, float size){
     for(int i = 0; i < nbPoissons; i++){
-        double posX = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
-        double posY = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
-        double posZ = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
+        float posX = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
+        float posY = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
+        float posZ = (rangeNeg + (rand() % static_cast<int>(rangePos - rangeNeg + 1)))/coefDivision;
 
         QVector3D position(posX, posY, posZ);
         Poisson p(position, vitesse, size);
         vecPoisson.push_back(p);
     }
+}
+
+void Banc::anime(){
+
 }
--- banc.h
+++ banc.h
@@ -11,15 +11,20 @@ class Banc
 public:
     Banc();
     void initPoissons(QVector3D vitesse, float size);
+    void anime();
 
 
     int nbPoissons = 0;
     std::vector<Poisson> vecPoisson;
 
+    double largeurAquarium = 10.0;
+    double hauteurAquarium = 10.0;
+    double profondeurAquarium = 10.0;
+
 
 protected:
-    double rangePos = 1000.0;
-    double rangeNeg = -1000.0;
+    float rangePos = 1000.0;
+    float rangeNeg = -1000.0;
     int coefDivision = 100;
 };
 
